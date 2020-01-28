a1355c6d289b333bd21bbaa96a98b4e542613d20
Paskowsky <Paskowsky@users.noreply.github.com>
2020-01-28 00:57:06 -0800

    First commit!
    First public commit.

---
+++ CachedField.Tests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CachedField.Tests
+{
+    class Program
+    {
+        static CachedField<string> x;
+
+        static void Main(string[] args)
+        {
+            x = new CachedField<string>(() => { Thread.Sleep(5000); return DateTime.Now.ToString(); }, new TimeSpan(0, 0, 30));
+
+            new Thread(() => { while (true) { Thread.Sleep(10000); x.Invalidate(); } }).Start();
+            new Thread(() => { while (true) { Thread.Sleep(20000); x.Invalidate(); } }).Start();
+
+            while (true)
+            {
+                Console.WriteLine(x.Value);
+
+                Thread.Sleep(2 * 1000);
+
+            }
+        }
+
+
+    }
+}
---
+++ CachedField/CachedField.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CachedField
+{
+    public class CachedField<T>
+    {
+        public static readonly TimeSpan DefaultUpdateSpan = new TimeSpan(0, 5, 0);
+
+        private T value;
+        private Func<T> getValueDelegate;
+        private ReaderWriterLockSlim rwLock;
+        private DateTime lastUpdate;
+
+        private readonly TimeSpan updateSpan;
+
+
+        private bool InvalidateRequired()
+        {
+            bool result = false;
+            rwLock.EnterReadLock();
+            if (DateTime.Now.Subtract(lastUpdate) > updateSpan)
+            {
+                result = true;
+            }
+            rwLock.ExitReadLock();
+            return result;
+        }
+
+        public CachedField(Func<T> getValueDelegate) : this(getValueDelegate, DefaultUpdateSpan)
+        {
+
+        }
+
+        public CachedField(Func<T> getValueDelegate, TimeSpan updateSpan)
+        {
+            if (getValueDelegate == null)
+                throw new ArgumentNullException("getValueDelegate");
+
+            this.rwLock = new ReaderWriterLockSlim();
+
+            this.updateSpan = updateSpan;
+            this.getValueDelegate = getValueDelegate;
+
+            this.Invalidate();
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (InvalidateRequired())
+                {
+                    Invalidate();
+                }
+
+                //Enter read lock to prevent value writing
+                rwLock.EnterReadLock();
+                T temp = value;
+                rwLock.ExitReadLock();
+
+                return temp;
+            }
+        }
+        
+        public void Invalidate()
+        {
+            if (!rwLock.TryEnterWriteLock(1))
+            {
+                //If it's already updating cache, don't update again.
+                return;
+            }
+
+            value = getValueDelegate();
+            lastUpdate = DateTime.Now;
+            rwLock.ExitWriteLock();
+        }
+    }
+}
